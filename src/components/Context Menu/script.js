ff62eaff8950f6acf416fc0fc92049b067c65ab9
Vikram-Hegde <vikramhegde1440@gmail.com>
2022-07-14 10:56:33 +0530

    almost done 😓

--- src/components/Context Menu/script.js
+++ src/components/Context Menu/script.js
@@ -17,43 +17,71 @@ document.addEventListener('contextmenu', (e) => {
 		top: e.clientY,
 		left: e.clientX,
 		right: 'auto',
+		toggleLR: 1,
 	};
 	let thresholdW = menuW + e.clientX >= parseInt(width);
 	let thresholdWOptions = menuW * 2 + e.clientX >= parseInt(width);
 	let thresholdH = menuH + e.clientY >= parseInt(height);
+	let x = 0,
+		y = 0;
 	if (thresholdW) {
 		pos = {
 			top: e.clientY,
-			right: 0,
+			right: width - e.clientX,
 			left: 'auto',
+			toggleLR: -1,
 		};
-	} else {
-		menu.className = 'menu left-anim';
+		x = 100;
+		y = 0;
 	}
 	if (thresholdWOptions) {
-		menu.className = 'menu right-anim';
+		pos.toggleLR = -1;
 	}
 	if (thresholdH) {
 		pos = {
 			top: e.clientY - menuH,
 			left: e.clientX,
 			right: 'auto',
+			toggleLR: 1,
 		};
+		x = 0;
+		y = 100;
 	}
 	if (thresholdH && thresholdW) {
 		pos = {
 			top: e.clientY - menuH,
 			left: 'auto',
-			right: 0,
+			right: width - e.clientX,
+			toggleLR: -1,
 		};
+		x = 100;
+		y = 100;
 	}
 	menu.setAttribute(
 		'style',
-		`display: flex; top: ${pos.top}px; left: ${pos.left}px; right: ${pos.right}px`
+		`display: flex; --toggle: ${pos.toggleLR}; top: ${pos.top}px; left: ${pos.left}px; right: ${pos.right}px`
+	);
+	menu.animate(
+		[
+			{ clipPath: `circle(0% at ${x}% ${y}%` },
+			{ clipPath: `circle(210% at ${x}% ${y}%` },
+		],
+		{
+			duration: 250,
+			easing: 'ease-in-out',
+		}
 	);
 });
 
 document.addEventListener('click', (e) => {
 	if (menu.contains(e.target)) return;
-	menu.style.display = 'none';
+	menu
+		.animate(
+			[
+				{ transform: 'scale(1) translateY(0)', opacity: 1 },
+				{ transform: 'scale(0.9) translateY(20px)', opacity: 0 },
+			],
+			{ duration: 100, easing: 'ease-out' }
+		)
+		.finished.then(() => (menu.style.display = 'none'));
 });
