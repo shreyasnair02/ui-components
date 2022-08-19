9de21af05dc486c18e3ca3e435b165997c1d39cc
Vikram-Hegde <vikramhegde1440@gmail.com>
2022-08-19 10:38:00 +0530

    initial commit

--- src/components/Tabs/script.ts
+++ src/components/Tabs/script.ts
@@ -1,32 +1,35 @@
-const tabsWrapper: HTMLDivElement = document.querySelector('.tabs');
-const tabs: NodeListOf<HTMLButtonElement> = document.querySelectorAll('.tab');
-const tabContent: NodeListOf<HTMLDivElement> = document.querySelectorAll('.tab-content');
-const highlighter: HTMLDivElement = document.querySelector('.highlighter');
-let bool = false;
+class Tabs {
+	private highlighter: HTMLDivElement;
+	private tabContent: NodeListOf<HTMLDivElement>;
+	private tabs: NodeListOf<HTMLButtonElement>;
+	private tabWrapper: HTMLDivElement;
+	private firstBounds: DOMRect;
 
-tabsWrapper.addEventListener('mouseover', (e) => {
-    let elem = e.target as HTMLButtonElement;
-    highlighter.style.opacity = '1';
-    if(elem.classList.contains('tab') && !bool) {
-        let bounds = elem.getBoundingClientRect();
-        highlighter.setAttribute('style', `height: ${bounds.height}px; width: ${bounds.width}px; top: ${bounds.top}px; left: ${bounds.left}px;`)
-        bool = true;
-    } else if (bool && elem.classList.contains('tab')) {
-        let updatedBounds = elem.getBoundingClientRect();
-        highlighter.style.left = `${updatedBounds.left}px`;
-        highlighter.style.width = `${updatedBounds.width}px`;
-    }
-})
+	constructor() {
+		this.tabWrapper = document.querySelector('.tabs');
+		this.tabs = document.querySelectorAll('.tab');
+		this.tabContent = document.querySelectorAll('.tab-content');
+		this.highlighter = document.querySelector('.highlighter');
 
-tabsWrapper.addEventListener('mouseleave', () => {
-    bool = false;
-    highlighter.style.opacity = '0';
-})
+		this.addEventListeners();
+	}
 
-tabs.forEach((tab, idx) => {
-    tab.addEventListener('click', () => {
-       const active: HTMLDivElement = document.querySelector('.show');
-       active.classList.remove('show');
-       tabContent[idx].classList.add('show');
-    })
-})
+	addEventListeners() {
+		this.tabs.forEach(( tab, idx  )=> tab.addEventListener('click', () => this.switchTab(idx)))
+		this.setInitialBounds();
+	}
+
+	setInitialBounds() {
+		// set bounds
+		this.firstBounds = this.tabs[0].getBoundingClientRect();
+		this.highlighter.setAttribute('style', `top: ${this.firstBounds.top}px; left: ${this.firstBounds.left}px; height: ${this.firstBounds.height}px`)
+	}
+
+	switchTab(idx) {
+		const active: HTMLDivElement = document.querySelector('.show');
+		active.classList.remove('show');
+		this.tabContent[idx].classList.add('show');
+	}
+}
+
+new Tabs();
