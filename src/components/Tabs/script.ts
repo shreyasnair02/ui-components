e96235243c27a5f5b65c800f7a4d6662463cb283
Vikram-Hegde <vikramhegde1440@gmail.com>
2022-08-19 17:31:01 +0530

    removed some unused code

--- src/components/Tabs/script.ts
+++ src/components/Tabs/script.ts
@@ -5,8 +5,6 @@ class Tabs {
 	private tabWrapper: HTMLDivElement;
 	private firstBounds: DOMRect;
 	private left: number;
-	private bool: boolean;
-	private previousTarget: HTMLButtonElement;
 
 	constructor() {
 		this.tabWrapper = document.querySelector('.tabs');
@@ -14,12 +12,9 @@ class Tabs {
 		this.tabContent = document.querySelectorAll('.tab-content');
 		this.highlighter = document.querySelector('.highlighter');
 		this.left = 0;
-		this.bool = false;
-		this.previousTarget = null;
 
 		// bindings, changing context of _this_
 		this.mouseover = this.mouseover.bind(this);
-		this.mouseenter = this.mouseenter.bind(this);
 
 		this.addEventListeners();
 	}
@@ -31,21 +26,18 @@ class Tabs {
 		this.setInitialBounds();
 		this.tabWrapper.addEventListener('mouseover', this.mouseover);
 		this.tabWrapper.addEventListener('mouseleave', () => {
-			this.highlighter.style.opacity = '0';
+			this.highlighter.style.opacity = '0'
 		});
 		this.tabWrapper.addEventListener('mouseenter', () => {
-			this.highlighter.style.transition = 'transform 0ms, opacity 150ms';
+			this.highlighter.classList.remove('transition');
+			this.highlighter.style.opacity = '1'
 		});
 	}
 
 	mouseover(e) {
 		if (!e.target.classList.contains('tab')) return;
-		this.highlighter.style.opacity = '1';
-		this.highlighter.style.transitionProperty = 'transform, opacity, width';
-		this.highlighter.style.transitionTimingFunction = 'ease-in-out'
-		this.highlighter.style.transitionDuration = '150ms';
+		this.highlighter.classList.add('transition');
 		const { target } = e;
-		this.previousTarget = target;
 		const { width, left } = target.getBoundingClientRect();
 		const updatedBounds = {
 			deltaX: left - this.left,
@@ -54,10 +46,6 @@ class Tabs {
 		console.log(updatedBounds);
 		this.highlighter.style.width = `${updatedBounds.deltaW}px`;
 		this.highlighter.style.transform = `translateX(${updatedBounds.deltaX}px)`;
-	}
-
-	mouseenter(e) {
-		this.highlighter.style.transitionDuration = '0ms';
 	}
 
 	setInitialBounds() {
