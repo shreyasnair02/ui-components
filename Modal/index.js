d9b9644f28c05d9787c21cd98f11c3333a7c5439
shreyasnair02 <SHREYAS.20201ISE0085@presidencyuniversity.in>
2022-08-29 00:24:01 +0530

    fnial

---
+++ Modal/index.js
@@ -0,0 +1,47 @@
+let open = document.querySelector('.modal_btn');
+let section = document.querySelector('.section');
+let modal = document.querySelector('.modal');
+let idname = document.querySelector('#idname');
+let pass = document.querySelector('#idpass');
+let email = document.querySelector('#idemail');
+
+open.addEventListener('click', (e) => {
+    e.stopPropagation();
+    change();
+
+    document.body.addEventListener('click', takeInput);
+});
+
+function change() {
+    document.body.removeEventListener('click', takeInput);
+    modal.classList.toggle('hidden');
+    section.classList.toggle('darken');
+}
+
+
+function takeInput(e) {
+    e.stopPropagation();
+    let form = document.querySelector('.details_section');
+    let formData = new FormData(form);
+    if (e.target.closest('.shut') || (!e.target.closest('.modal'))) {
+        form.reset();
+        change();
+
+    }
+
+    else if (e.target.closest('.signin')) {
+        if (form.checkValidity()) {
+            console.log('true')
+            change();
+        }
+        else {
+            console.log('false')
+            form.reportValidity();
+
+        }
+    }
+    else if (e.target.closest('.reset')) {
+        form.reset();
+    }
+
+}
