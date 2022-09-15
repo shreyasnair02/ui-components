eb5d0284538846b1a5a4a9ec8d150e41d33a0025
Vikram-Hegde <vikramhegde1440@gmail.com>
2022-09-15 22:45:01 +0530

    baseline toast

--- src/components/Toast/script.js
+++ src/components/Toast/script.js
@@ -1,7 +1,33 @@
-class ToastWC extends HTMLElement {
-	constructor() {
-		super()
+// @ts-check
 
-		this.shadowRoot({ mode: 'open' })
+class UIToast extends HTMLElement {
+	constructor() {
+		super();
+		/** 
+		 * Toast Template
+		 * @type {HTMLTemplateElement | null}  
+		*/
+		this.template = document.querySelector('#toast-template');
+	}
+
+	connectedCallback() {
+		this.attachShadow({ mode: 'open' })
+		if(this.template)
+		this.shadowRoot?.append(this.template.content.cloneNode(true));
+
+		let closeBtn = this.shadowRoot?.querySelector('.close-toast');
+		closeBtn?.addEventListener('click', () => {
+			this.remove();
+		})
 	}
 }
+
+customElements.define('ui-toast', UIToast)
+
+const button = document.querySelector('.add-toast');
+const toastContainer = document.querySelector('.toast-container');
+
+button?.addEventListener('click', () => {
+	let toast = document.createElement('ui-toast');
+	toastContainer?.append(toast);
+})
