cafc4154370a02df85e09e779ee53b060b6a4337
Vikram-Hegde <vikramhegde1440@gmail.com>
2022-09-15 09:02:06 +0530

    no semicolons 💀

--- src/components/Context Menu/script.js
+++ src/components/Context Menu/script.js
@@ -1,37 +1,37 @@
-let menu = document.querySelector('.menu');
-let touchDevice = window.matchMedia('(hover: none), (pointer: coarse)');
+let menu = document.querySelector('.menu')
+let touchDevice = window.matchMedia('(hover: none), (pointer: coarse)')
 
 document.addEventListener('contextmenu', (e) => {
-	e.preventDefault();
-	menu.style.display = 'flex';
-	if (touchDevice.matches) return;
+	e.preventDefault()
+	menu.style.display = 'flex'
+	if (touchDevice.matches) return
 
 	const width =
 		window.innerWidth ||
 		document.documentElement.clientWidth ||
-		document.body.clientWidth;
+		document.body.clientWidth
 	const height =
 		window.innerHeight ||
 		document.documentElement.clientHeight ||
-		document.body.clientHeight;
+		document.body.clientHeight
 
-	let menuDimensions = menu.getBoundingClientRect();
+	let menuDimensions = menu.getBoundingClientRect()
 	let menuW = menuDimensions.width,
-		menuH = menuDimensions.height;
+		menuH = menuDimensions.height
 
 	let pos = {
 		top: e.clientY,
 		left: e.clientX,
 		right: 'auto',
 		toggleLR: 1,
-	};
+	}
 
-	let thresholdW = menuW + e.clientX >= parseInt(width);
-	let thresholdWOptions = menuW * 2 + e.clientX >= parseInt(width);
-	let thresholdH = menuH + e.clientY >= parseInt(height);
+	let thresholdW = menuW + e.clientX >= parseInt(width)
+	let thresholdWOptions = menuW * 2 + e.clientX >= parseInt(width)
+	let thresholdH = menuH + e.clientY >= parseInt(height)
 
 	let x = 0,
-		y = 0;
+		y = 0
 
 	if (thresholdH && thresholdW) {
 		pos = {
@@ -39,33 +39,33 @@ document.addEventListener('contextmenu', (e) => {
 			left: 'auto',
 			right: width - e.clientX,
 			toggleLR: -1,
-		};
-		x = 100;
-		y = 100;
+		}
+		x = 100
+		y = 100
 	} else if (thresholdW) {
 		pos = {
 			top: e.clientY,
 			left: 'auto',
 			right: width - e.clientX,
 			toggleLR: -1,
-		};
-		x = 100;
+		}
+		x = 100
 	} else if (thresholdWOptions) {
-		pos.toggleLR = -1;
+		pos.toggleLR = -1
 	} else if (thresholdH) {
 		pos = {
 			top: e.clientY - menuH,
 			left: e.clientX,
 			right: 'auto',
 			toggleLR: 1,
-		};
-		y = 100;
+		}
+		y = 100
 	}
 
 	menu.setAttribute(
 		'style',
 		`display: flex; --toggle: ${pos.toggleLR}; top: ${pos.top}px; left: ${pos.left}px; right: ${pos.right}px`
-	);
+	)
 
 	menu.animate(
 		[
@@ -76,14 +76,14 @@ document.addEventListener('contextmenu', (e) => {
 			duration: 250,
 			easing: 'ease-in-out',
 		}
-	);
-});
+	)
+})
 
 document.addEventListener('click', (e) => {
-	if (menu.contains(e.target)) return;
-	let translateX = 0;
+	if (menu.contains(e.target)) return
+	let translateX = 0
 	if (touchDevice.matches) {
-		translateX = -50;
+		translateX = -50
 	}
 	menu
 		.animate(
@@ -93,5 +93,5 @@ document.addEventListener('click', (e) => {
 			],
 			{ duration: 100, easing: 'ease-in-out' }
 		)
-		.finished.then(() => (menu.style.display = 'none'));
-});
+		.finished.then(() => (menu.style.display = 'none'))
+})
--- src/components/Tabs/script.ts
+++ src/components/Tabs/script.ts
@@ -1,70 +1,71 @@
 class Tabs {
-	private highlighter: HTMLDivElement;
-	private tabContent: NodeListOf<HTMLDivElement>;
-	private tabs: NodeListOf<HTMLButtonElement>;
-	private tabWrapper: HTMLDivElement;
-	private firstBounds: DOMRect;
-	private left: number;
-	private previousIndex: number;
-	private tabContentParent: HTMLDivElement;
+	private highlighter: HTMLDivElement
+	private tabContent: NodeListOf<HTMLDivElement>
+	private tabs: NodeListOf<HTMLButtonElement>
+	private tabWrapper: HTMLDivElement
+	private firstBounds: DOMRect
+	private left: number
+	private previousIndex: number
+	private tabContentParent: HTMLDivElement
 
 	constructor() {
-		this.tabWrapper = document.querySelector('.tabs');
-		this.tabs = document.querySelectorAll('.tab');
-		this.tabContentParent = document.querySelector('.tabs-content');
-		this.tabContent = document.querySelectorAll('.tab-content');
-		this.highlighter = document.querySelector('.highlighter');
-		this.left = 0;
-		this.previousIndex = 0;
+		this.tabWrapper = document.querySelector('.tabs')
+		this.tabs = document.querySelectorAll('.tab')
+		this.tabContentParent = document.querySelector('.tabs-content')
+		this.tabContent = document.querySelectorAll('.tab-content')
+		this.highlighter = document.querySelector('.highlighter')
+		this.left = 0
+		this.previousIndex = 0
 
 		// bindings, changing context of _this_
-		this.mouseover = this.mouseover.bind(this);
-		this.setInitialBounds = this.setInitialBounds.bind(this);
+		this.mouseover = this.mouseover.bind(this)
+		this.setInitialBounds = this.setInitialBounds.bind(this)
 
-		this.addEventListeners();
+		this.addEventListeners()
 	}
 
 	addEventListeners() {
 		this.tabs.forEach((tab: HTMLButtonElement, idx: number) =>
 			tab.addEventListener('click', () => this.switchTab(idx))
-		);
-		this.setInitialBounds();
-		this.tabWrapper.addEventListener('mouseover', this.mouseover);
+		)
+		this.setInitialBounds()
+		this.tabWrapper.addEventListener('mouseover', this.mouseover)
 		this.tabWrapper.addEventListener('mouseleave', () => {
-			this.highlighter.style.opacity = '0';
-		});
+			this.highlighter.style.opacity = '0'
+		})
 		this.tabWrapper.addEventListener('mouseenter', () => {
-			this.highlighter.classList.remove('transition');
-			this.highlighter.style.opacity = '1';
-		});
-		window.addEventListener('resize', this.setInitialBounds);
+			this.highlighter.classList.remove('transition')
+			this.highlighter.style.opacity = '1'
+		})
+		window.addEventListener('resize', this.setInitialBounds)
 	}
 
 	mouseover(e) {
-		if (!e.target.classList.contains('tab')) return;
-		this.highlighter.classList.add('transition');
-		const { target } : { target: HTMLDivElement} = e;
-		const { width, left } : { width: number, left: number} = target.getBoundingClientRect();
+		if (!e.target.classList.contains('tab')) return
+		this.highlighter.classList.add('transition')
+		const { target }: { target: HTMLDivElement } = e
+		const { width, left }: { width: number; left: number } =
+			target.getBoundingClientRect()
 		const updatedBounds = {
 			deltaX: left - this.left,
 			deltaW: width,
-		};
-		this.highlighter.style.width = `${updatedBounds.deltaW}px`;
-		this.highlighter.style.transform = `translateX(${updatedBounds.deltaX}px)`;
+		}
+		this.highlighter.style.width = `${updatedBounds.deltaW}px`
+		this.highlighter.style.transform = `translateX(${updatedBounds.deltaX}px)`
 	}
 
 	setInitialBounds() {
-		this.firstBounds = this.tabs[0].getBoundingClientRect();
+		this.firstBounds = this.tabs[0].getBoundingClientRect()
 		this.highlighter.setAttribute(
 			'style',
 			`top: ${this.firstBounds.top}px; left: ${this.firstBounds.left}px; height: ${this.firstBounds.height}px;`
-		);
-		this.left = this.firstBounds.left;
+		)
+		this.left = this.firstBounds.left
 	}
 
 	switchTab(idx: number) {
-		this.handleActive(idx, 'tab--active', this.tabs);
-		this.handleActive(idx, 'show', this.tabContent);
+		this.handleActive(idx, 'tab--active', this.tabs)
+		this.handleActive(idx, 'show', this.tabContent)
 	}
 
 	handleActive(
@@ -72,18 +73,18 @@ class Tabs {
 		className: string,
 		elem: NodeListOf<HTMLButtonElement> | NodeListOf<HTMLDivElement>
 	) {
-		let getActive = document.querySelector(`.${className}`);
-		getActive?.classList.remove(className);
-		elem[idx].classList.add(className);
+		let getActive = document.querySelector(`.${className}`)
+		getActive?.classList.remove(className)
+		elem[idx].classList.add(className)
 		if (elem === this.tabs) {
 			if (this.previousIndex < idx) {
-				this.tabContentParent.style.setProperty('--translate', '-40px');
+				this.tabContentParent.style.setProperty('--translate', '-40px')
 			} else {
-				this.tabContentParent.style.setProperty('--translate', '40px');
+				this.tabContentParent.style.setProperty('--translate', '40px')
 			}
-			this.previousIndex = idx;
+			this.previousIndex = idx
 		}
 	}
 }
 
-new Tabs();
+new Tabs()
