6897678a9cf90a839d241841af5186d92a025575
Vikram-Hegde <vikramhegde1440@gmail.com>
2022-09-16 14:13:42 +0530

    removing after anim

--- src/components/Toast/script.js
+++ src/components/Toast/script.js
@@ -1,3 +1,16 @@
+const toastContainer = document.createElement('div')
+toastContainer.classList.add('toast-container')
+document.body.append(toastContainer)
+
+// array of positive messages
+const positiveMessages = [
+	'Great job!',
+	'You did it!',
+	'You are awesome!',
+	'You are the best!',
+	'You are the greatest!',
+]
+
 class UIToast extends HTMLElement {
 	constructor() {
 		super()
@@ -10,25 +23,35 @@ class UIToast extends HTMLElement {
 
 	connectedCallback() {
 		this.attachShadow({ mode: 'open' })
-		this.shadowRoot?.append(this.template.content.cloneNode(true))
+		this.shadowRoot.append(this.template.content.cloneNode(true))
+
+		let content = this.shadowRoot.querySelector('span')
+		content.textContent = this.dataset.content
+
+		this.shadowRoot.querySelector('output').addEventListener('animationend', () => {
+			this.remove();
+		})
 	}
 }
 
 customElements.define('ui-toast', UIToast)
 
+/** @type {HTMLButtonElement} */
 const button = document.querySelector('.add-toast')
-const toastContainer = document.querySelector('.toast-container')
 
-button?.addEventListener('click', () => {
+button.addEventListener('click', () => {
 	let toast = document.createElement('ui-toast')
-	if (toastContainer.childElementCount)
-		toastContainer
-			.animate([{ transform: 'translateY(-35px)' }], {
-				duration: 150,
-				easing: 'ease-in-out',
-			})
-			.finished.then(() => toastContainer.append(toast))
-	else {
-		toastContainer.append(toast)
-	}
+	toast.setAttribute(
+		'data-content',
+		positiveMessages[Math.floor(Math.random() * positiveMessages.length)]
+	)
+
+	toastContainer.childElementCount
+		? toastContainer
+				.animate([{ transform: 'translateY(-35px)' }], {
+					duration: 150,
+					easing: 'ease-in-out',
+				})
+				.finished.then(() => toastContainer.append(toast))
+		: toastContainer.append(toast)
 })
